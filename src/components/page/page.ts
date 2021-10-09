9e8f0cc75094c4db39c3ef0f88881e22ddbda195
wonkim-dev <wonkim.dev@gmail.com>
2021-10-10 00:06:10 +0200

    Refactor components: create base component

--- src/components/page/page.ts
+++ src/components/page/page.ts
@@ -1,14 +1,7 @@
-export class PageComponent {
-  private element: HTMLUListElement;
+import { BaseComponent } from "../component.js";
 
+export class PageComponent extends BaseComponent<HTMLUListElement> {
   constructor() {
-    this.element = document.createElement("ul");
-    this.element.setAttribute("class", "page");
-    this.element.textContent = "This is PageComponent";
-  }
-
-  // Attach element to parent element at a given position
-  attachTo(parent: HTMLElement, position: InsertPosition = "afterbegin") {
-    parent.insertAdjacentElement(position, this.element);
+    super('<ul class="page">This is PageComponent!</ul>');
   }
 }

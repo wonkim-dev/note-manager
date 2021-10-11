b2534e68d0bcb69086a61f4481075a835e4924bf
wonkim-dev <wonkim.dev@gmail.com>
2021-10-11 21:57:24 +0200

    Build page item component

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
-import { PageComponent } from "./components/page/page.js";
+import { Composable, PageComponent } from "./components/page/page.js";
 import { ImageComponent } from "./components/page/item/image.js";
 import { NoteComponent } from "./components/page/item/note.js";
 import { TodoComponent } from "./components/page/item/todo.js";
 import { VideoComponent } from "./components/page/item/video.js";
+import { Component } from "./components/component.js";
 
 class App {
-  private readonly page: PageComponent;
+  private readonly page: Component & Composable;
   constructor(appRoot: HTMLElement) {
     this.page = new PageComponent();
     this.page.attachTo(appRoot);
@@ -14,19 +15,19 @@ class App {
       "Image Title",
       "http://picsum.photos/300/150"
     );
-    image.attachTo(appRoot, "beforeend");
-
-    const note = new NoteComponent("Note title", "This is the first note.");
-    note.attachTo(appRoot, "beforeend");
-
-    const todo = new TodoComponent("Todo Title", "Todo Item");
-    todo.attachTo(appRoot, "beforeend");
+    this.page.addChild(image);
 
     const video = new VideoComponent(
       "Video Title",
       "https://www.youtube.com/watch?v=p1w9EteDDeU"
     );
-    video.attachTo(appRoot, "beforeend");
+    this.page.addChild(video);
+
+    const note = new NoteComponent("Note title", "This is the first note.");
+    this.page.addChild(note);
+
+    const todo = new TodoComponent("Todo Title", "Todo Item");
+    this.page.addChild(todo);
   }
 }
 

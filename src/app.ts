9dba1989ed9c81b30f19a5fe7ac14560c6b41e88
wonkim-dev <wonkim.dev@gmail.com>
2021-10-13 21:09:02 +0200

    Inject page item component to the page component for testability

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,8 @@
-import { Composable, PageComponent } from "./components/page/page.js";
+import {
+  Composable,
+  PageComponent,
+  PageItemComponent,
+} from "./components/page/page.js";
 import { ImageComponent } from "./components/page/item/image.js";
 import { NoteComponent } from "./components/page/item/note.js";
 import { TodoComponent } from "./components/page/item/todo.js";
@@ -8,7 +12,7 @@ import { Component } from "./components/component.js";
 class App {
   private readonly page: Component & Composable;
   constructor(appRoot: HTMLElement) {
-    this.page = new PageComponent();
+    this.page = new PageComponent(PageItemComponent);
     this.page.attachTo(appRoot);
 
     const image = new ImageComponent(
